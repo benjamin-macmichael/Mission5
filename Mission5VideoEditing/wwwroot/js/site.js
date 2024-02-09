70a7d73018853dadb5f56c2153434a3b2e0af9f0
Ben Macmichael <benjaminmac21@gmail.com>
2024-02-08 22:03:26 -0700

    some more comments

--- Mission5VideoEditing/wwwroot/js/site.js
+++ Mission5VideoEditing/wwwroot/js/site.js
@@ -1,12 +1,21 @@
-﻿//This calculates how much money the user would have to pay for my services based off the
-//amount of hours they typed into the hours box
+﻿// Event listener for the click event on the "Calculate" button
 $("#calculateBtn").click(function () {
-        var hours = $("#hours").val();
-        if (hours > 0) {
-            var chargePerHour = $("#chargePerHour").val();
-            var total = hours * chargePerHour;
-            $("#total").val(total.toFixed(2));
-        } else {
-            alert("Please enter a valid number of hours.");
-        }
-    });
+    // Retrieve the value entered for the number of hours
+    var hours = $("#hours").val();
+
+    // Check if the entered value is a positive number
+    if (hours > 0) {
+        // Retrieve the charge per hour value
+        var chargePerHour = $("#chargePerHour").val();
+
+        // Calculate the total charge based on entered hours and charge per hour
+        var total = hours * chargePerHour;
+
+        // Display the calculated total in the corresponding input field, rounded to 2 decimal places
+        $("#total").val(total.toFixed(2));
+    } else {
+        // If the entered value is not a valid positive number, show an alert
+        alert("Please enter a valid number of hours.");
+    }
+});
+
