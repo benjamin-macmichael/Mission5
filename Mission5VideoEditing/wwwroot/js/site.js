3538537db0df471323c0d919114d2108da903846
Ben Macmichael <benjaminmac21@gmail.com>
2024-02-08 21:47:34 -0700

    Add project files.

---
+++ Mission5VideoEditing/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿$("#calculateBtn").click(function () {
+        var hours = $("#hours").val();
+        if (hours > 0) {
+            var chargePerHour = $("#chargePerHour").val();
+            var total = hours * chargePerHour;
+            $("#total").val(total.toFixed(2));
+        } else {
+            alert("Please enter a valid number of hours.");
+        }
+    });
