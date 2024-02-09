6026b272fde89d7be6af86eb11e5668dbf779771
Ben Macmichael <benjaminmac21@gmail.com>
2024-02-08 21:57:47 -0700

    added comments

--- Mission5VideoEditing/Controllers/HomeController.cs
+++ Mission5VideoEditing/Controllers/HomeController.cs
@@ -1,32 +1,41 @@
+// Import necessary namespaces
 using Microsoft.AspNetCore.Mvc;
 using Mission5VideoEditing.Models;
 using System.Diagnostics;
 
+// Declare the controller class for handling Home-related actions
 namespace Mission5VideoEditing.Controllers
 {
     public class HomeController : Controller
     {
+        // Logger to capture and handle errors or information
         private readonly ILogger<HomeController> _logger;
 
+        // Constructor to inject the logger into the controller
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
+        // Action method for the default home page (Index)
         public IActionResult Index()
         {
             return View();
         }
 
+        //Services page
         public IActionResult Services()
         {
             return View();
         }
 
+        // Action method to handle and display errors
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            // Create an ErrorViewModel to pass error details to the view
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
+
--- Mission5VideoEditing/wwwroot/js/site.js
+++ Mission5VideoEditing/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿$("#calculateBtn").click(function () {
+﻿//This calculates how much money the user would have to pay for my services based off the
+//amount of hours they typed into the hours box
+$("#calculateBtn").click(function () {
         var hours = $("#hours").val();
         if (hours > 0) {
             var chargePerHour = $("#chargePerHour").val();
